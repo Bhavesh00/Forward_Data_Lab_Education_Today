f9e21ac337d578c3f0e1d2c51c05d7ea191945f3
rohanp3 <rohanp3@illinois.edu>
2021-06-24 22:02:10 -0700

    implemented a infor extractor for h2 tags

--- professor_scrap.py
+++ professor_scrap.py
@@ -1,23 +1,13 @@
+from __future__ import print_function
+from pprint import pprint
+
 from googlesearch import search # https://pypi.org/project/googlesearch-python/
-import pandas as pd
 
 '''
 Go to Google. 
 Search (name,institution). 
 Filter results --> URLs that are faculty homepages.
 '''
-# to search
-professor_name = "Kevin Chen-Chuan Chang"
-instituition = "UIUC"
-
-# query = professor_name + " " + instituition
-  
-# for j in search(query, lang="en", num_results=5):
-#     print(j)
-
-
-prof_list = [(professor_name,instituition),("Jiawei Han",instituition)]
-
 def query_urls(prof_list):
     
     for p_name, p_instituition in prof_list:
@@ -25,7 +15,11 @@ def query_urls(prof_list):
 
         print (search(query, lang="en", num_results=10))
         print ("\n")
+# to search
+professor_name = "Kevin Chen-Chuan Chang"
+instituition = "UIUC"
 
+prof_list = [(professor_name,instituition),("Jiawei Han",instituition)]
 
 '''
 Go through each page. 
@@ -36,17 +30,63 @@ Extract information:
     -Research interests
     -Publications
 '''
-
 def extract_information(p_url):
+    import requests
+    from bs4 import BeautifulSoup
+
     information_criteria = ["Biography", "Education","Awards", "Research interests", "Publications"]
 
 
 
+    headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+    url = p_url
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    target = soup.find("h2",text = "Biography")
+    i = 0
+    if target:
+        for sib in target.find_next_siblings():
+            if sib.name == "h2":
+                break
+            else:
+                print("\n",sib.text)
 
 
+
+def google_knowledge_graph(query):
+    """Example of Python client calling Knowledge Graph Search API."""
+    import json
+    import urllib
+
+    # query_urls(prof_list)
+    #api_key = open('.api_key').read()
+    api_key = "AIzaSyC26XthU72YUZjxf_0nK50UTmCPfoO7vzw"
+    #query = "Jiawei Han"
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+        'query': query,
+        'limit': 20,
+        'indent': True,
+        'key': api_key,
+    }
+    url = service_url + '?' + urllib.parse.urlencode(params)
+    response = json.loads(urllib.request.urlopen(url).read())
+    for element in response['itemListElement']:
+        pprint(element)
+        exit()
+        print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
 
 def main():
-    query_urls(prof_list)
+    test_url = "https://cs.illinois.edu/about/people/faculty/kcchang"
+    extract_information(test_url)
+
 
 if __name__ == "__main__":
     main()
